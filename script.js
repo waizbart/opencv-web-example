e0aa589490993bc765be639f6055e1f874892be1
Guilherme Waizbart <63511690+waizbart@users.noreply.github.com>
2022-04-26 16:20:41 -0300

    video

--- script.js
+++ script.js
@@ -2,57 +2,55 @@ const video = document.getElementById("video-input");
 const canvas = document.getElementById("canvas-output");
 
 (async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: false,
-  });
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+    });
 
-  let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-  let cap = new cv.VideoCapture(video);
+    video.srcObject = stream;
+    video.play();
 
-  if (!stream) {
-    src.delete();
-    dst.delete();
-    return;
-  }
-
-  video.srcObject = stream;
-  video.play();
-
-  const FPS = 30;
-  function processVideo() {
-    let begin = Date.now();
-    cap.read(src);
-
-    // processamento
+    let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
     let gray = new cv.Mat();
-    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
+    let cap = new cv.VideoCapture(video);
+    let faces = new cv.RectVector()
+    console.log(faces)
+    let classifier = new cv.CascadeClassifier();
+    let utils = new Utils();
+    let faceCascadeFile = 'haarcascade_frontalface_default.xml';
 
-    let thresh = new cv.Mat();
-    cv.threshold(gray, thresh, 90, 255, cv.THRESH_OTSU);
+    utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
+        classifier.load(faceCascadeFile);
+    });
 
-    let hierarchy = new cv.Mat();
-    let contours = new cv.MatVector();
+    const FPS = 30;
 
-    cv.findContours(
-      thresh,
-      contours,
-      hierarchy,
-      cv.RETR_CCOMP,
-      cv.CHAIN_APPROX_SIMPLE
-    );
+    function processVideo() {
+        let begin = Date.now();
 
-    for (let i = 0; i < contours.size(); ++i) {
-      let color = new cv.Scalar(255, 0, 0);
-      cv.drawContours(src, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+        cap.read(src);
+
+        cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+
+        try {
+            classifier.detectMultiScale(gray, faces, 1.1, 2, 0);
+        } catch (err) {
+            console.log(err);
+        }
+        for (let i = 0; i < faces.size(); ++i) {
+            let face = faces.get(i);
+            let point1 = new cv.Point(face.x, face.y);
+            let point2 = new cv.Point(face.x + face.width, face.y + face.height);
+            cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
+        }
+
+        cv.imshow("canvas-output", src);
+
+        // atualização de frames
+        let delay = 1000 / FPS - (Date.now() - begin);
+
+        setTimeout(processVideo, delay);
     }
 
-    cv.imshow("canvas-output", src);
-
-    // atualização de frames
-    let delay = 1000 / FPS - (Date.now() - begin);
-    setTimeout(processVideo, delay);
-  }
-
-  setTimeout(processVideo, 0);
-})();
+    setTimeout(processVideo, 0);
+})()
